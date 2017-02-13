2c79ad66611badf41500812b2e765393f94e506c
Phil <Philip.Shaughnessy@gmail.com>
2017-02-13 11:24:05 -0500

    made fizzbuzz

--- test.rb
+++ test.rb
@@ -1,2 +1,11 @@
 #this is mostly git stuff
 puts 'hello world'
+
+(1..100).each do |num|
+  puts case
+        when num%15 == 0 then "FizzBuzz"
+        when num%5 == 0  then "Fizz"
+        when num%3 == 0  then "Buzz"
+        else num
+        end
+end
